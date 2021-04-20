f2e9110fbd772dc44d79e7c5a1cd99e6f158467f
= <riyavaryani01@gmail.com>
2021-04-20 21:16:55 +0530

    add transaction done

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState,useContext } from 'react';
+import {GlobalContext} from '../Context/GlobalContext';
 
 export const AddTransaction = () => {
 const [text, setText] = useState('');
 const [amount, setAmount] = useState('0');
 
+const {addTransaction} = useContext(GlobalContext);
+var i = 5;
+
+const onSubmit = e => {
+    e.preventDefault();
+    
+
+    const newTransaction = {
+        id: i,
+        text, amount: +amount
+    }
+    i=i+1;
+    addTransaction(newTransaction);
+}
+
     return (
         <>
             <h3>Add Transaction</h3>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
                     <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
